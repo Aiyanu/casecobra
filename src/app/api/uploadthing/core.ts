9f1efd96cd70dd41c58f13af130f4f319a3a9827
Iyanuoluwapo Adesanya <Iyanuoluwapo Adesanya>
2024-07-03 00:44:06 +0100

    uploading project

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,54 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { z } from "zod";
+import sharp from "sharp";
+import { db } from "@/db";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
+    .input(z.object({ configId: z.string().optional() }))
+    .middleware(async ({ input }) => {
+      return { input };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // console.log("Image uploaded");
+
+      const { configId } = metadata.input;
+
+      const res = await fetch(file.url);
+      const buffer = await res.arrayBuffer();
+
+      const imgMetadata = await sharp(buffer).metadata();
+      // console.log("metadata image", imgMetadata);
+
+      const { width, height } = imgMetadata;
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        });
+        // console.log(configuration);
+
+        return { configId: configuration.id };
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        });
+        console.log(updatedConfiguration);
+
+        return { configId: updatedConfiguration.id };
+      }
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
